3fe9157b43b1a17b18ceac395cd8a73fda303537
AlejandroLiinares <alejandrolinaresnt@gmail.com>
2026-02-19 01:23:17 -0300

    Call to action updated: h2 changes

--- src/components/calltoaction/CallToAction.tsx
+++ src/components/calltoaction/CallToAction.tsx
@@ -3,7 +3,10 @@ import styles from './CallToAction.module.css';
 const CallToAction = () => {
   return (
     <section className={styles.section}>
-      <h2 className={styles.h2Title}>Hagámoslo realidad.</h2>
+      <h2 className={styles.h2Title}> 
+       <span className={styles.h2Cursive}> Contáctame</span>{' '}
+       y construyamos soluciones de otro nivel, hoy.
+      </h2>
     </section>
   );
 };

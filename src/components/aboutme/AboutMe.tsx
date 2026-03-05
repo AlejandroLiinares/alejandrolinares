4341ecce5a4ee9314d4beececf5dd08132118c8a
Alejandro <alejandrolinaresnt@gmail.com>
2026-03-05 14:29:47 -0300

    Imports from AboutMe, about to code it

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -1,11 +1,12 @@
 import styles from './AboutMe.module.css';
-import BentoGridMe from './bentogridme/BentoGridMe';
+
+import myselfIcon    from '../../assets/aboutme/myself.jpg';
+
 
 const AboutMe = () => {
   return (
     <section className={styles.section}>
       <h2 className={styles.h2Title}>Alejandro Linares</h2>
-      <BentoGridMe />
     </section>
   );
 };
